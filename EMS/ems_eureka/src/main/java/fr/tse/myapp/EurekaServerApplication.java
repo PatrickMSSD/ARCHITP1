d8916f717b7b921b0baf9130c024200780ef6b75
Patrick <patmassaad@hotmail.com>
2020-02-21 05:08:29 +0100

    "Mis en place d'eureka, manque ribbon et zuul"

--- EMS/ems_eureka/src/main/java/fr/tse/myapp/EurekaServerApplication.java
+++ EMS/ems_eureka/src/main/java/fr/tse/myapp/EurekaServerApplication.java
@@ -2,15 +2,15 @@ package fr.tse.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @SpringBootApplication
-@EnableEurekaClient
-public class App {
+@EnableEurekaServer
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
