1a11caed35314d78934458410d672f6e36053a90
Patrick <patmassaad@hotmail.com>
2020-02-21 05:19:02 +0100

    "Ajout de ribbon, manque zuul"

--- MS/ms_collection/src/main/java/fr/tse/myapp/App.java
+++ MS/ms_collection/src/main/java/fr/tse/myapp/App.java
@@ -3,10 +3,12 @@ package fr.tse.myapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "ms_collection")
 public class App {
 
 	public static void main(String[] args) {
